4e6513b0f984110734af42c68951efbbed78bc9a
vitor-hi <santanna.2060@gmail.com>
2025-03-12 05:29:08 -0300

    Recuperação de conta

---
+++ README.md
@@ -0,0 +1,3 @@
+# clone-tabnews
+
+Aprendendo a fazer um projeto completo do zero.
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home() {
+  return <h1>Tecnologia alienígena</h1>;
+}
+
+function Teste() {
+  return <h1>Teste</h1>;
+}
+
+export default Home;
